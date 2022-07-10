ab77d4c4ee8790eb93aa84fb51e2a4893a351bd9
Yhaizley Restrepo <yhazrestrepo@gmail.com>
2022-07-10 18:08:48 -0500

    Interfaces

---
+++ main/script.js
@@ -0,0 +1,41 @@
+
+/*const body=document.querySelector(".body"),
+      sidebar=body.querySelector(".nav"),
+      sidebar_toggle =body.querySelector(".sidebar_toggle ");
+
+
+      sidebar_toggle.addEventListener("click", ()=>{
+      sidebar.classList.toggle("close");
+      });*/
+/*
+document.getElementById("btn_open").addEventListener("click",open_close_menu);
+
+var side_menu = document.getElementById("side_menu");
+var btn_open = document.getElementById("btn_open");
+var body = document.getElementById("body");
+
+
+function open_close_menu(){
+    body.classList.toggle("body_move");
+    side_menu.classList.toggle("menu__side_move");
+}*/
+/*
+document.getElementById("btn_open").addEventListener("click",open_close_menu);
+
+const body=document.querySelector("#body"),
+      sidebar=document.querySelector("#sidebar"),
+      sidebar_toggle =docuement.querySelector("#sidebar_toggle");
+
+
+function open_close_menu(){
+    body.classList.toggle("body_move");
+    sidebar_toggle.classList.toggle("menu__side_move");
+}*/
+   
+    
+
+
+
+
+
+     
